0d94c1d9e17d024bf61136daceec484fdc47b088
mongilteacher <mongilteacher@gmail.com>
2025-04-22 13:53:28 +0900

    총알 완료

--- Assets/02.Scripts/Player/PlayerFire.cs
+++ Assets/02.Scripts/Player/PlayerFire.cs
@@ -3,8 +3,7 @@ using UnityEngine;
 
 public class PlayerFire : MonoBehaviour
 {
-  
-
+    
     // 필요 속성
     // - 발사 위치
     public GameObject FirePosition;
@@ -12,7 +11,11 @@ public class PlayerFire : MonoBehaviour
     public GameObject BombPrefab;
     // - 던지는 힘
     public float ThrowPower = 15f;
-
+    
+    
+    // 목표: 마우스의 왼쪽 버튼을 누르면 카메라가 바라보는 방향으로 총을 발사하고 싶다.
+    public ParticleSystem BulletEffect;
+    
     private void Start()
     {
         Cursor.lockState = CursorLockMode.Locked; 
@@ -34,7 +37,31 @@ public class PlayerFire : MonoBehaviour
             bombRigidbody.AddTorque(Vector3.one);
         }
         
-        
+        // 총알 발사(레이저 방식)
+        // 1. 왼쪽 버튼 입력 받기
+        if (Input.GetMouseButtonDown(0))
+        {
+            // 2. 레이를 생성하고 발사 위치와 진행 방향을 설정
+            Ray ray = new Ray(FirePosition.transform.position, Camera.main.transform.forward);
+            
+            // 3. 레이와 부딛힌 물체의 정보를 저장할 변수를 생성
+            RaycastHit hitInfo = new RaycastHit();
+
+            // 4. 레이를 발사한 다음,            
+            bool isHit = Physics.Raycast(ray, out hitInfo);
+            if (isHit) //데이터가 있다면(부딛혔다면
+            {
+                // 피격 이펙트 생성(표시)
+                BulletEffect.transform.position = hitInfo.point;
+                BulletEffect.transform.forward  = hitInfo.normal; // 법선 벡터: 직선에 대하여 수직인 벡터
+                BulletEffect.Play();
+                
+                // 게임 수학: 선형대수학(스칼라, 벡터, 행렬) 기하학(삼각함수 ..)
+            }
+        }
+        // Ray: 레이저(시작위치, 방향)
+        // RayCast: 레이저를 발사
+        // RayCastHit: 레이저가 물체와 부딛혔다면 그 정보를 저장하는 구조체
         
     }
 }
