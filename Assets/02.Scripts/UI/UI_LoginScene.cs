60157e2a1870574c6ace71ba751d31d3e53fbbaf
mongilteacher <mongilteacher@gmail.com>
2025-05-09 15:25:15 +0900

    소금치기

--- Assets/02.Scripts/UI/UI_LoginScene.cs
+++ Assets/02.Scripts/UI/UI_LoginScene.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Security.Cryptography;
+using System.Text;
 using TMPro;
 using UnityEngine.UI;
 using UnityEngine;
@@ -24,7 +26,8 @@ public class UI_LoginScene : MonoBehaviour
     
     [Header("회원가입")] 
     public UI_InputFields RegisterInputFields;
-   
+
+    private const string SALT = "10043420";
     
     
 
@@ -60,7 +63,6 @@ public class UI_LoginScene : MonoBehaviour
         if (string.IsNullOrEmpty(id))
         {
             RegisterInputFields.ResultText.text = "아이디를 입력해주세요.";
-            // (ResultText 흔들어주세요.)
             return;
         }
         
@@ -71,7 +73,6 @@ public class UI_LoginScene : MonoBehaviour
             RegisterInputFields.ResultText.text = "비밀번호를 입력해주세요.";
             return;
         }
-        
         
         // 3. 2차 비밀번호 입력을 확인하고, 1차 비밀번호 입력과 같은지 확인한다.
         string password2 = RegisterInputFields.PasswordComfirmInputField.text;
@@ -89,11 +90,31 @@ public class UI_LoginScene : MonoBehaviour
         
         // 4. PlayerPrefs를 이용해서 아이디와 비밀번호를 저장한다.
         // (비밀번호를 암호화 해서 저장하세요.)
-        PlayerPrefs.SetString(id, password);
+        PlayerPrefs.SetString(id, Encryption(password + SALT));
         
         // 5. 로그인 창으로 돌아간다.
         // (이때 아이디는 자동 입력되어 있다.)
         OnClickGoToLoginButton();
+    }
+
+    public string Encryption(string text)
+    {
+        // 해시 암호화 알고리즘 인스턴스를 생성한다.
+        SHA256 sha256 = SHA256.Create();
+        
+        // 운영체제 혹은 프로그래밍 언어별로 string 표현하는 방식이 다 다르므로
+        // UTF8 버전 바이트로 배열로 바꿔야한다.
+        byte[] bytes = Encoding.UTF8.GetBytes(text);
+        byte[] hash = sha256.ComputeHash(bytes);
+        
+        string resultText = string.Empty;
+        foreach (byte b in hash)
+        {
+            // byte를 다시 string으로 바꿔서 이어붙이기
+            resultText += b.ToString("X2");
+        }
+
+        return resultText;
     }
     
 }
